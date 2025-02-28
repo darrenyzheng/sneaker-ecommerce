d98763a4eb3c0eb5967a89f9526151500b8938dd
Darren Zheng <darrenzheng1997@gmail.com>
2025-02-27 19:45:34 -0800

    changing for vercel

--- ecommerce/src/components/Shop/Shop.jsx
+++ ecommerce/src/components/Shop/Shop.jsx
@@ -9,8 +9,8 @@ const Shop = () => {
     // Dynamically set the API base URL based on environment
     const apiUrl = import.meta.env.MODE === 'development'
     ? 'http://localhost:5000/api'  // Local API during development
-    : 'https://sneaker-ecommerce-khaki.vercel.app/api';  // Absolute URL in production
-    
+    : '/api';  // Absolute URL in production
+
     useEffect(() => {
         fetch(`${apiUrl}/shop`, { method: 'GET' })
             .then((response) => {
