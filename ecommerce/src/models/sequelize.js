7ed492af1527d1e71eb1085205c493d6be31269b
Darren Zheng <darrenzheng1997@gmail.com>
2025-02-28 19:38:01 -0800

    changing for vercel

--- ecommerce/src/models/sequelize.js
+++ ecommerce/src/models/sequelize.js
@@ -3,13 +3,14 @@ import UserModel from './user.model.js';
 import SneakerModel from './sneaker.model.js';
 import TransactionModel from './transaction.model.js';
 import dotenv from 'dotenv';
-
+import pg from 'pg';
 dotenv.config();
 
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
 const sequelize = new Sequelize('postgres', 'postgres', POSTGRES_PASSWORD, {
     host: 'sneaker-ecommerce.c1iekokmyd0e.us-east-2.rds.amazonaws.com',
     dialect: 'postgres',
+    dialectModule: pg,
     port: 5432, 
     ssl: true,  
     dialectOptions: {
